989a79c51225f2427285c864cb2e2b4a1e76312b
Vir-Web <82693755+Vir-Web@users.noreply.github.com>
2021-04-17 11:23:36 +0630

    Update send_multiple_sms.php

--- examples/send_multiple_sms.php
+++ examples/send_multiple_sms.php
@@ -12,8 +12,8 @@ $apiClient = new SMSGatewayApi(AUTH_KEY, SERVER);
 try {
 
     $mobile_numbers = array(
-        '11110346122',
-        '11113595747',
+        '+959123456789',
+        '+959123456789',
     );
 
     $message = 'do you like sport?';
@@ -49,11 +49,11 @@ Array
         (
             [1] => Array
                 (
-                    [schedule_at] => 2020-06-04 22:03:48
+                    [schedule_at] => 2021-04-04 22:03:48
                     [queue_id] => 1591286628c9f3
                     [device_id] => 1
                     [sim_id] => 99
-                    [mobile_no] => 11110346122
+                    [mobile_no] => +959123456789
                     [data_type] => Plain
                     [message] => do you like sport?
                     [status] => Failed
@@ -62,14 +62,14 @@ Array
 
             [2] => Array
                 (
-                    [schedule_at] => 2020-06-04 22:03:48
+                    [schedule_at] => 2021-04-04 22:03:48
                     [queue_id] => 159128662854f3
                     [device_id] => 1
                     [sim_id] => 99
-                    [mobile_no] => 11113595747
+                    [mobile_no] => +959123456789
                     [data_type] => Plain
                     [message] => do you like sport?
-                    [created_at] => 2020-06-04 22:03:48
+                    [created_at] => 2021-04-04 22:03:48
                     [status] => Success
                     [response_text] => Message Successfully Send for Processing
                 )
@@ -94,8 +94,8 @@ $apiClient = new SMSGatewayApi(AUTH_KEY, SERVER);
 try {
 
  $mobile_numbers = array(
-        '11110346122',
-        '11113595747',
+        '+959123456789',
+        '+959123456789',
     );
     $message = 'Do you like sport?';
     $sender_id = 'dfewrty56yu';
@@ -126,28 +126,28 @@ Array
         (
             [1] => Array
                 (
-                    [schedule_at] => 2020-06-04 21:21:55
+                    [schedule_at] => 2021-04-04 21:21:55
                     [queue_id] => 1591284115b4f3
                     [gateway] => mimsms
                     [sender_id] => dfewrty56yu
-                    [mobile_no] => 11110346122
+                    [mobile_no] => +959123456789
                     [data_type] => Plain
                     [message] => Do you like sport?
-                    [created_at] => 2020-06-04 21:21:55
+                    [created_at] => 2021-04-04 21:21:55
                     [status] => Failed
                     [response_text] => Mobile Number was Blocked
                 )
 
             [2] => Array
                 (
-                    [schedule_at] => 2020-06-04 21:21:55
+                    [schedule_at] => 2021-04-04 21:21:55
                     [queue_id] => 15912841157673
                     [gateway] => mimsms
                     [sender_id] => dfewrty56yu
-                    [mobile_no] => 11113595747
+                    [mobile_no] => +959123456789
                     [data_type] => Plain
                     [message] => Do you like sport?
-                    [created_at] => 2020-06-04 21:21:55
+                    [created_at] => 2021-04-04 21:21:55
                     [status] => Success
                     [response_text] => Message Successfully Send for Processing
                 )
