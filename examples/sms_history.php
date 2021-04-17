eb38ee7347a96dc3c7adb75c0fab4f79f1e0f1c3
Vir-Web <82693755+Vir-Web@users.noreply.github.com>
2021-04-17 11:27:20 +0630

    Update sms_history.php

--- examples/sms_history.php
+++ examples/sms_history.php
@@ -11,7 +11,7 @@ $apiClient = new SMSGatewayApi(AUTH_KEY, SERVER);
 
 try {
     
-    $history = $apiClient->getHistory(array('filterby_send_through' => 'Http', 'filterby_gateway_id' => 'mimsms', 'filterby_from' => '2019-11-29 12:00:00', 'filterby_to' => '2020-11-29 23:59:00'));
+    $history = $apiClient->getHistory(array('filterby_send_through' => 'Http', 'filterby_gateway_id' => 'mimsms', 'filterby_from' => '2021-04-06 12:00:00', 'filterby_to' => '2021-04-16 23:59:00'));
 
     print_r($history);
 
@@ -36,20 +36,20 @@ Array
                     [id] => 93
                     [send_through] => Http
                     [gateway_id] => mimsms
-                    [sender_id] => wed63478u
+                    [sender_id] => virwebsms
                     [country_id] => 14
                     [response_id] => 957593
                     [status] => Delivered
                     [device_id] => 
                     [sim_id] => 
-                    [mobile_no] => 11110346122
+                    [mobile_no] => +959123456789
                     [data_type] => Plain
                     [message] => This is very good morning...
                     [campaign_id] => 
                     [user_id] => 3
                     [created_by] => 3
-                    [updated_at] => 2020-05-30 17:41:32
-                    [created_at] => 2020-05-30 17:41:01
+                    [updated_at] => 2021-04-06 17:41:32
+                    [created_at] => 2021-04-06 17:41:01
                 )
 
         )
